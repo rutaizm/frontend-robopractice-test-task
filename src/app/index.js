c60c50b36d3138af4f6e20c325e38adbca588b00
Tatyana Izmailova <rutaizm@yandex.ru>
2022-10-22 19:17:51 +0300

    fow data filter finished

--- src/app/index.js
+++ src/app/index.js
@@ -16,37 +16,65 @@ export const App = () => {
           user.id = item.id;
           user.fullname=item.Fullname;         
           
-          // for (let i=1; i<32; i++) {
-          //   user[i] = 0;
-          // }
+          for (let i=1; i<32; i++) {
+            user[i] = 0;          
           
-          item.Days.forEach((day) => {  
-           user[getMonth(day.Date)] = getTime(day);            
-          });
+            item.Days.forEach((day) => {  
+              return user[getMonth(day.Date)] = getTime(day.Start, day.End); 
+            });
+          }
+          user.total = formatTotal(getTotal(user)) 
           return console.log(user) 
       })
       return filteredData
     }
 
-    function getTime(oneDay) {
-        const start = oneDay.Start.split("-");
-        const end = oneDay.End.split("-");
-        const startingTime = new Date(0, 0, 0, start[0], start[1], 0);
-        const endingTime = new Date(0, 0, 0, end[0], end[1], 0);
-        let diff = endingTime.getTime() - startingTime.getTime();
-        const hours = Math.floor(diff / 1000 / 60 / 60);
-        diff -= hours * 1000 * 60 * 60;
-        const minutes = Math.floor(diff / 1000 / 60);
-
-        return `${hours}:${minutes < 10 ? '0' : ''}${minutes}`
+    function timeConverter(rawData) {
+      const data = rawData.split(/(?:-|:)+/);
+      const timeFormat = new Date(0, 0, 0, data[0], data[1], 0);
+      const time = timeFormat.getTime();
+      return time
     }
 
+    function formatTime(times) {
+      const num = times.split(':').map(Number);
+      let t = 60 * num[0] + num[1]
+      return t 
+    }
+     
+
+    function getTime(start, end) {
+      const startingTime = timeConverter(start)
+      const endingTime = timeConverter(end)
+      let diff = endingTime - startingTime;
+      const hours = Math.floor(diff / 1000 / 60 / 60);
+      diff -= hours * 1000 * 60 * 60;
+      const minutes = Math.floor(diff / 1000 / 60);
+    
+      return `${hours}:${minutes < 10 ? '0' : ''}${minutes}`       
+    }
+
+    function formatTotal(num) {
+      const hours = Math.floor(num / 60);     
+      const minutes = Math.floor(num %60);
+      return `${hours}:${minutes < 10 ? '0' : ''}${minutes}`
+    }
+   
     function getMonth(month) {
         const newMonth = new Date(month);
         return newMonth.getDate();
     }
 
+    function getTotal(user) {
+      let total = 0;
+      for (let i=1;i <= 31; i++) {
+        if (user[i] === 0) continue
+        total += formatTime(user[i]) 
+      }      
+      return total;
+    }
 
+ 
 
     React.useEffect(() => {
       getInitialData()
